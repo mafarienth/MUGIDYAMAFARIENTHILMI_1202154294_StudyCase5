e0bff5b33478a90ed349006ace39e5302338065a
mafarienth <mugidya.mafarienth@gmail.com>
2018-03-25 22:36:46 +0700

    push kedua kali

---
+++ app/src/main/java/com/example/arien/mugidyamafarienthilmi_1202154294_studycase5/AddData.java
@@ -0,0 +1,42 @@
+package com.example.arien.mugidyamafarienthilmi_1202154294_studycase5;
+
+/**
+ * Created by ASUS on 3/23/2018.
+ */
+
+public class AddData {
+    //deklarasi variable
+    String todo, desc, prior;
+
+    //konstruktor
+    public AddData(String todo, String desc, String prior){
+        this.todo=todo;
+        this.desc=desc;
+        this.prior=prior;
+    }
+
+    //method setter dan getter untuk to do , description dan priority
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPrior() {
+        return prior;
+    }
+
+    public void setPrior(String prior) {
+        this.prior = prior;
+    }
+}
